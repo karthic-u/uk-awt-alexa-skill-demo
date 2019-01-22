5e4decc5f10aedec5278b15b86b277a76e8012b7
karthic-u <karthic.u@gmail.com>
2019-01-22 17:12:58 +0530

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$text = "Hello World, this is my first Alexa Skill";
-
-$array =array("body"=>array("version"=>"1.0", "response"=>array("outputSpeech"=>array("type"=>"plainText", "text"=>$text))));
-
-echo json_encode($array);
-?>
